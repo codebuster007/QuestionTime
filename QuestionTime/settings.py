490929e696e053b5dca0fbdeac42e1f1934b0082
JOMADI <victorjo603@gmail.com>
2020-01-06 12:26:28 +0100

    Add: Changed Debug options in settings.py

--- QuestionTime/settings.py
+++ QuestionTime/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', '')
+DEBUG = bool(os.environ.get('DEBUG', 'True'))
 
-if not DEBUG:
+if DEBUG:
     try:
         import local_settings as ls
 
